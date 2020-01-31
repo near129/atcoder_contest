3fd886817e160d93bba38a43d6f650590108c479
near129 <ryou.12912@gmail.com>
2020-01-31 20:08:21 +0900

    osモジュールからpathlibに置き換え サンプルケースをテキストファイルに保存する機能を追加

--- atcoder_contest.py
+++ atcoder_contest.py
@@ -1,6 +1,5 @@
 import sys
-import os
-import pathlib
+from pathlib import Path
 import pickle
 import requests
 from bs4 import BeautifulSoup
@@ -17,14 +16,15 @@ PYTEST_TEMPLATE = [
     ')',
     'def test_1(IN, OUT):',
     '    cp = subprocess.run(["python", "Main.py"], encoding="utf-8", ',
-    '                        input=IN, stdout=subprocess.PIPE)',
-    '                        cwd=os.path.dirname(__file__)'
+    '                        input=IN, stdout=subprocess.PIPE),',
+    '                        cwd=os.path.dirname(__file__))'
     '    assert cp.stdout == OUT',
     ''
 ]
 ATCODER_URL = 'https://atcoder.jp/'
 CONTEST_INFO = {}
-PATH = os.path.dirname(__file__)
+# PATH = os.path.dirname(__file__)
+CURRENT_PATH = Path(__file__).parent
 
 
 def login():
@@ -48,26 +48,30 @@ def login():
         print('Login failed!')
         exit()
     print(BeautifulSoup(res.text, 'lxml').select('div.alert')[0].text)
-    with open(os.path.join(PATH, 'cookie.pkl'), 'wb') as f:
+    # pickle化
+    with open(CURRENT_PATH / Path('cookie.pkl'), 'wb') as f:
         pickle.dump(res.cookies, f)
 
 
-def is_login(cookies):  # cookiesが有効どうか確認する
-    res = requests.get(ATCODER_URL+'settings', cookies=cookies)
+def is_login(cookies):
+    # cookiesが有効どうか確認する
+    res = requests.get(ATCODER_URL + 'settings', cookies=cookies)
     setting_url = ATCODER_URL + 'settings'
     return res.url == setting_url
 
 
-def get_contest_info(contest_name):
-    if not os.path.isfile(os.path.join(PATH, 'cookie.pkl')):
+def get_contest_info(contest_id):
+    CONTEST_INFO['contest_id'] = contest_id
+    # 保存したcookieの取得
+    if not (CURRENT_PATH / Path('cookie.pkl')).exists():
         login()
-    with open(os.path.join(PATH, 'cookie.pkl'), 'rb') as f:
+    with open(CURRENT_PATH / Path('cookie.pkl'), 'rb') as f:
         cookies = pickle.load(f)
-    if not(is_login(cookies)):
+    if not (is_login(cookies)):
         print('cookies is invalid \nPlease login again')
         login()
     # atcoderの印刷用問題のページから取得
-    res = requests.get(ATCODER_URL+f'contests/{contest_name}/tasks_print',
+    res = requests.get(ATCODER_URL + f'contests/{contest_id}/tasks_print',
                        cookies=cookies)
     soup = BeautifulSoup(res.text, 'lxml')
     title = soup.title.text
@@ -76,38 +80,51 @@ def get_contest_info(contest_name):
         exit()
     CONTEST_INFO['name'] = title.replace(' ', '')
     problem_names = soup.select('span.h2')
-    CONTEST_INFO['problem_names'] = list(map(lambda x: x.text.replace(' ', '')
-                                             , problem_names))
-    for name in CONTEST_INFO['problem_names']:
-        CONTEST_INFO[name] = {}
-    problem_io_styles = soup.select('.lang-ja > .io-style')
+    CONTEST_INFO['problem_names'] = list(map(lambda x: x.text.replace(' ', ''),
+                                             problem_names))
+    # for name in CONTEST_INFO['problem_names']:
+    #         CONTEST_INFO[name] = {}
+    # problem_io_styles = soup.select('.lang-ja > .io-style')
     # for name, io in zip(problem_names, problem_io_styles):
-    for name, io in zip(problem_names, soup.select('.lang-ja')):
+    for name, io in zip(CONTEST_INFO['problem_names'], soup.select('.lang-ja')):
+        CONTEST_INFO[name] = {}
         examples = io.select('.io-style ~ div')
-        name = name.text.replace(' ', '')
-        if not('例' in list(map(lambda x: x.h3.text[2], examples))):
+        # しっかりサンプルケースを取得しているか確認
+        if not ('例' in list(map(lambda x: x.h3.text[2], examples))):
             print('Could not get test cases!')
             exit()
         CONTEST_INFO[name]['IN'] = [e.pre.text for e in examples[::2]]
         CONTEST_INFO[name]['OUT'] = [e.pre.text for e in examples[1::2]]
-    print(title)
-    print(*['\t' + pn.text for pn in problem_names], sep='\n')
+    print('-' * 32, title, *['\t' + pn.text for pn in problem_names],
+          '-' * 32, sep='\n')
+
+
+# デバックなど用に入力例のテキストファイルを作成する(リダイレクション)
+def make_testcase_text(directory, problem_name):
+    directory /= Path('IN')
+    directory.mkdir()
+    for num, IN in enumerate(CONTEST_INFO[problem_name]['IN']):
+        with open(directory / Path(f'{num+1:03}'), 'w') as f:
+            f.write(IN)
 
 
 def make_test_file_directory():
-    contest_name = CONTEST_INFO['name']
-    pathlib.Path(contest_name).mkdir()
+    contest_id = CONTEST_INFO['contest_id']
+    contest_dir = Path(contest_id)
+    contest_dir.mkdir()
     for i, name in enumerate(CONTEST_INFO['problem_names']):
-        directory = f'{contest_name}/{name}'
-        pathlib.Path(directory).mkdir(parents=True)
-        param = ',\n'.join([f'{" "*8}("""{i}""", """{o}""")'for i, o in
-                            zip(CONTEST_INFO[name]['IN'],
-                                CONTEST_INFO[name]['OUT'])])
-        with open(directory + f'/test_{chr(97 + i)}.py', 'w') as f:
-            f.write('\n'.join(PYTEST_TEMPLATE[:6] + [param] +
-                              PYTEST_TEMPLATE[6:]))
-        with open(directory + '/Main.py', 'w'):
+        directory = contest_dir / Path(chr(97 + i))
+        directory.mkdir(parents=True)
+        example = ',\n'.join([f'{" " * 8}("""{i}""", """{o}""")' for i, o in
+                              zip(CONTEST_INFO[name]['IN'],
+                                  CONTEST_INFO[name]['OUT'])])
+        with open(directory / Path(f'test_{chr(97 + i)}.py'), 'w') as f:
+            f.write('\n'.join(PYTEST_TEMPLATE[:7] + [example] +
+                              PYTEST_TEMPLATE[7:]))
+        with open(directory / Path('Main.py'), 'w'):
             pass
+        make_testcase_text(directory, name)
+    print('Files created successfully!!')
 
 
 def main():
